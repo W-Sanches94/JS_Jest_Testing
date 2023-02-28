65a671390d609377d85373c2c5b709ac5f79cbed
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-02-28 15:22:22 +0000

    completed sum-modulo test

--- calculator.test.js
+++ calculator.test.js
@@ -16,15 +16,22 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 42000;
+    actual = sum(20000, 22000);
+    expect(actual).toBe(expected); 
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    expected = -5;
+    actual = sum(-2, -3);
+    expect(actual).toBe(expected); 
   });
 
-  test.skip('can add zero', () => {
+  test('can add zero', () => {
+    expected = 5;
+    actual = sum(5,0);
+    expect(actual).toBe(expected); 
     
   });
 
@@ -32,18 +39,63 @@ describe('sum', () => {
 
 describe('subtract', () => {
 
+  test('can subtract two small positive numbers', () => {
+    expected = 6;
+    actual = subtract(9, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract two large positive numbers', () => { 
+    expected = 60000;
+    actual = subtract(80000, 20000);
+    expect(actual).toBe(expected); 
+  });
+
 });
 
 describe('multiply', () => {
 
+  test('can multiply two small positive numbers', () => {
+    expected = 49;
+    actual = multiply(7, 7);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply two large positive numbers', () => { 
+    expected = 43680;
+    actual = multiply(160, 273);
+    expect(actual).toBe(expected); 
+  });
+
 });
 
 describe('divide', () => {
+  test('can divide two small positive numbers', () => {
+    expected = 3;
+    actual = divide(9, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test('can divide two large positive numbers', () => { 
+    expected = 31;
+    actual = divide(62000, 2000);
+    expect(actual).toBe(expected); 
+  });
 
 });
 
 describe('modulus', () => {
+  test('can modulus two small positive numbers', () => {
+    expected = 2;
+    actual = modulus(2, 3);
+    expect(actual).toBe(expected);
+  });
 
+  test('can modulus two large positive numbers', () => { 
+    expected = 20000;
+    actual = modulus(20000, 22000);
+    expect(actual).toBe(expected); 
+  });
 });
 
 describe('even', () => {
